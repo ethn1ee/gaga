21a1a0fd280329dd231e7da295190206be64760b
Ethan Lee <ethantlee21@gmail.com>
2025-06-25 01:31:39 -0400

    remove /new page from middleware and add serverside auth check

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
 import { username } from "better-auth/plugins";
 
 const prisma = new PrismaClient();
@@ -13,5 +14,5 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [username()],
+  plugins: [username(), nextCookies()],
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -16,5 +16,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   runtime: "nodejs",
-  matcher: ["/new", "/profile"],
+  matcher: ["/profile"],
 };
