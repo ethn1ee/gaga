9907cd0814529b4ea3dd579838587a2f9feee928
Ethan Lee <ethantlee21@gmail.com>
2025-07-08 00:08:46 -0400

    ignore vercel analytics routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -35,5 +35,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   runtime: "nodejs",
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|_vercel).*)"],
 };
